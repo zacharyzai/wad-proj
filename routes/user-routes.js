c01fa0723b189149c7dc6753abaf0cf17ca213f3
kylie-kiaying <kylie.kiayingchua@gmail.com>
2026-03-22 02:07:06 +0800

    add mounted user routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user-controller');
-const isAuthenticated = require('../middleware/authMiddleware');
 
-// View profile
-router.get('/profile', isAuthenticated, userController.getProfile);
+router.get('/', userController.getProfile);
 
-// Edit profile page
-router.get('/profile/edit', isAuthenticated, userController.getEditProfile);
+router.get('/edit', userController.getEditProfile);
 
-// Update profile (CRUD)
-router.post('/profile/edit', isAuthenticated, userController.updateProfile);
+router.post('/edit', userController.updateProfile);
 
 module.exports = router;
