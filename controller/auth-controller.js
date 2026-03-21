14fbb4667ae32e074d4fc2b3c1ac530ba814d9ce
eugeniayj <yujia.wu.2025@computing.smu.edu.sg>
2026-03-21 23:38:47 +0800

    added errors validation

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,12 +1,25 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user-models');
 exports.registerGet = (req, res) => {
-    res.render('register');
+    res.render('register', { errors });
 }
 
 exports.registerPost = async (req, res) => {
     const { name, email, password, role } = req.body;
+    const errors = [];
+    if (!name || name.trim().length === 0) {
+        errors.push('Name is required.');
+    }
+    if (!email || !email.includes('@')) {
+        errors.push('Please enter a valid email');
+    }
+    if (!password || password.trim().length < 6 ) {
+        errors.push('Passwords requires minimum of 6 characters');
+    }
 
+    if (errors.length > 0) {
+        return res.render('register', { errors });
+    }
     try {
         const passwordHash = await bcrypt.hash(password, 10);
         await User.createUser({ name, email, passwordHash, role: role || 'student', studentId: '', faculty: '', bio: '' });
