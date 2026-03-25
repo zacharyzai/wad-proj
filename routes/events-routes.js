1f4dac31c58556c36a606cf3b2a6e30344b738b9
zz2309 <zachary.zai.2025@smu.edu.sg>
2026-03-25 15:50:28 +0800

    added review route

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -27,8 +27,7 @@ router.post("/delete-event", eventController.deleteEvent);
 // View Event Details
 router.get("/:id", eventController.viewEventDetails);
 
-// Add Review to Event
-router.post("/:id/review", eventController.addReview);
+
 
 
 module.exports = router;
